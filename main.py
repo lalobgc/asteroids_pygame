dde0ef5285d8042fb6035515030ed3b3401e99ff
lalobgc <lalobg.c@gmail.com>
2025-02-06 13:27:49 -0600

    dt and clock added, now the game runs without using all system's resources

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
