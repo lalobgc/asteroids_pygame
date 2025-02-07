f971fc93fd44a5323e9ff01398862d1629097996
lalobgc <lalobg.c@gmail.com>
2025-02-06 20:02:43 -0600

    Player motion and rotation. Groups updatable and drawable created, all works the same but with groups

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,12 +14,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+
+        ## player.update(dt)
+        ## player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
